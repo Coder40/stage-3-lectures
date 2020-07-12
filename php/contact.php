26fbe2758a1bbb555d18c8b7327087958691cbfd
Mohamed Jawad <m.almuahy404@gmail.com>
2020-07-12 17:32:32 +0300

    add file

---
+++ php/contact.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$email_from = $visitor_email;
+
+$email_subject = "Stage 3 Lectures";
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"User Message: $message.\n";
+
+
+$to = "coder.md.4@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: ../index.html");
+
+
+
+
+?>
